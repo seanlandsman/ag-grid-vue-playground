b52bf384bf240fc22a33cb5c3051ebf6759f9f76
Sean Landsman <sean@thelandsmans.com>
2018-12-10 15:43:02 +0000

    AG-2209 Automatically update Vue component params on refresh

---
+++ src/components/AutoRefreshCellRenderer.vue
@@ -0,0 +1,21 @@
+<template>
+    <div >
+        <div>{{ params.value }}</div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'AutoRefreshCellRenderer',
+        methods: {
+            /* autoParamsRefresh is equivalent to the following:
+            refresh(params) {
+                this.params = params;
+                return false;
+            } */
+        },
+        beforeDestroy() {
+            console.log("beforeDestroy");
+        }
+    }
+</script>
